ae73d7d5f886820d604c86eb1804ea5b4f58fa33
adnil-codes <lindachukwu961@gmail.com>
2022-07-17 22:08:43 +0100

    Updating the monty interpreter codes

---
+++ op_pint.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void op_pint(stack_t **stack, unsigned int line_number)
+{
+    if ((*stack) == NULL)
+    {
+        free_stack(stack)
+    }
+}
---
+++ op_push.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void op_push(stack_t **stack, unsigned int line_number, char *arg)
+{
+    if (!is_numeric(arg))
+    {
+        free_stack(stack);
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    push(stack, atoi(arg));
+}
